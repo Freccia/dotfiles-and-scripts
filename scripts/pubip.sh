a06273d2a74b75fd5ea02cabceb91adccd8d55ec
Freccia <Freccia@users.noreply.github.com>
2017-08-02 12:30:55 +0200

    pubip links

--- scripts/pubip.sh
+++ scripts/pubip.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+
 wget http://ipecho.net/plain -O - -q ; echo
+
+if [ $# -gt 1 ];then
+		if [ $1 == "-full" ];then
+				curl http://ipleak.com/full-report > ipleak.html
+		else
+				echo "Usage: ./pubip.sh [-full]"
+		fi
+fi
