035e9c8582614ab92ec83a35ee9d46bc23dd8f53
Freccia <Freccia@users.noreply.github.com>
2017-08-02 15:35:27 +0200

    modifs ++ vpnfailsafe

--- scripts/pubip.sh
+++ scripts/pubip.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+#
+#		Alternatives
+#    http://www.ifconfig.me
+#    http://www.icanhazip.com
+#    http://ipecho.net/plain
+#    http://ipogre.com
+#    http://indent.me
+#    http://bot.whatismyipaddress.com
+#
 
 wget http://ipecho.net/plain -O - -q ; echo
 
-if [ $# -gt 1 ];then
+
+if [ $# -gt 0 ];then
 		if [ $1 == "-full" ];then
 				curl http://ipleak.com/full-report > ipleak.html
 		else
